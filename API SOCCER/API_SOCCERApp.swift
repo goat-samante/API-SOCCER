79ad8e3dbdc68f4197de83fddc9c1edc845a434f
Samante <samante@bsd220.org>
2025-04-13 19:24:49 -0500

    Initial Commit

---
+++ API SOCCER/API_SOCCERApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_SOCCERApp.swift
+//  API SOCCER
+//
+//  Created by Samuel Amante on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct API_SOCCERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API SOCCERTests/API_SOCCERTests.swift
@@ -0,0 +1,17 @@
+//
+//  API_SOCCERTests.swift
+//  API SOCCERTests
+//
+//  Created by Samuel Amante on 4/13/25.
+//
+
+import Testing
+@testable import API_SOCCER
+
+struct API_SOCCERTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
